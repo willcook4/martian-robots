66c40626b6d98a7fd10b8c618e1278444fb6ec56
Will Cook <hello@doubleucook.com>
2018-08-02 19:25:39 +0100

    got the substring of movements

--- index.js
+++ index.js
@@ -14,10 +14,20 @@ RFRFRFRF
 1 1 E
 
 */
-
+/* Setup "Landing" */
 sampleInput = sampleInput.replace(/\s/g, '')
 console.log('sampleInput: ', sampleInput)
 let gridWorldWidth = sampleInput[0]
 // console.log('gridWorldWidth:', gridWorldWidth);
 let gridWorldHeight = sampleInput[1]
 // console.log('gridWorldHeight: ', gridWorldHeight);
+let robotStartX = sampleInput[2]
+console.log('robotStartX: ', robotStartX);
+let robotStartY = sampleInput[3]
+console.log('robotStartY: ', robotStartY);
+let robotStartDirection = sampleInput[4]
+console.log('robotStartDirection: ', robotStartDirection);
+
+// First movement
+let movementInstructions = sampleInput.substring(5)
+console.log('movementInstructions: ', movementInstructions);
