68e1892db0300e1e7c47f850b5ebd141b4fc36ab
Will Cook <hello@doubleucook.com>
2018-08-02 20:27:47 +0100

    working example with first sample instructions

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ Sample input:
 5 3
 1 1 E
 RFRFRFRF
+
+RF RF RF RF
 */
 let sampleInput = `
 5 3
@@ -35,8 +37,8 @@ let movementHistory = [] // store the last coordinates and direction
 
 // push in the start position
 movementHistory.push({
-  xPos: robotStartX,
-  yPos: robotStartY,
+  xPos: parseInt(robotStartX),
+  yPos: parseInt(robotStartY),
   direction: robotStartDirection
 })
 
@@ -90,36 +92,77 @@ let _parseNewDirection = (lastDirection, instructionDirection) => {
   return newDirection
 }
 
+let _updateLocation = (previousLocation, direction) => {
+  console.log('previousLocation: ', previousLocation)
+  console.log('direction: ', direction)
+  let newLocation
+  switch (direction) {
+    case 'N':
+      // y++
+      newLocation = previousLocation
+      newLocation.yPos = parseInt(previousLocation.yPos) + 1
+      console.log('New location is +1 y? ', newLocation)
+      break
+    case 'E':
+      // x++
+      newLocation = previousLocation
+      newLocation.xPos = parseInt(previousLocation.xPos) + 1
+      console.log('New location is +1 x? ', newLocation)
+      break
+    case 'S':
+      // y--
+      newLocation = previousLocation
+      newLocation.yPos = parseInt(previousLocation.yPos) - 1
+      console.log('New location is -1 Y? ', newLocation)
+      break
+    case 'W':
+      // x--
+      newLocation = previousLocation
+      newLocation.xPos = parseInt(previousLocation.xPos) - 1
+      console.log('New location is -1 X? ', newLocation)
+      break
+  }
+  console.log('returning newlocation: ', newLocation)
+  return newLocation
+}
+
 movementInstructions.map((instruction, index) => {
   console.log(`${index}: `, instruction)
   switch (instruction) {
     case 'R':
       console.log('Right turn')
       movementHistory.push({
-        xPos: movementHistory[0].xPos, // last xPos
-        yPos: movementHistory[0].yPos, // last xPos
+        xPos: movementHistory[movementHistory.length - 1].xPos, // last xPos
+        yPos: movementHistory[movementHistory.length - 1].yPos, // last xPos
         direction: _parseNewDirection(movementHistory[movementHistory.length - 1].direction, instruction)
       })
       break
     case 'L':
       console.log('Left turn')
       movementHistory.push({
-        xPos: movementHistory[0].xPos, // last xPos
-        yPos: movementHistory[0].yPos, // last xPos
+        xPos: movementHistory[movementHistory.length - 1].xPos, // last xPos
+        yPos: movementHistory[movementHistory.length - 1].yPos, // last xPos
         direction: _parseNewDirection(movementHistory[movementHistory.length - 1].direction, instruction)
       })
       break
     case 'F':
       console.log('Move "forward"')
+      // Detect if the robot has gone off the edge of the planet
+      let newLocation = _updateLocation({
+        xPos: movementHistory[movementHistory.length - 1].xPos,
+        yPos: movementHistory[movementHistory.length - 1].yPos},
+      movementHistory[movementHistory.length - 1].direction)
+
       movementHistory.push({
-        xPos: null, // last xPos
-        yPos: null, // last xPos
-        direction: movementHistory[0].direction
+        xPos: newLocation.xPos, // last xPos
+        yPos: newLocation.yPos, // last xPos
+        direction: movementHistory[movementHistory.length - 1].direction
       })
       break
   }
 
-  console.log('Last move: ', movementHistory[movementHistory.length - 1])
+  console.log('Last position: ', movementHistory[movementHistory.length - 1])
+  console.log(' ')
 })
 
 /* TODOs
