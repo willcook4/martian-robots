b8e1105c5d16d6ce81d0afaa75e2d5e314ff5127
Will Cook <hello@doubleucook.com>
2018-08-02 19:18:31 +0100

    stripped out the whitespace in the input string

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 /*
 Sample input:
-
 5 3
 1 1 E
 RFRFRFRF
 */
+let sampleInput = `
+5 3
+1 1 E
+RFRFRFRF
+`
 
 /* Sample Output
 1 1 E
 
 */
+
+sampleInput = sampleInput.replace(/\s/g, '')
+console.log('sampleInput: ', sampleInput)
+let gridWorldWidth = sampleInput[0]
+// console.log('gridWorldWidth:', gridWorldWidth);
+let gridWorldHeight = sampleInput[1]
+// console.log('gridWorldHeight: ', gridWorldHeight);
