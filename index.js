2740b63d9bf0f7782170d7b87db9ed2bc6a470d5
Will Cook <hello@doubleucook.com>
2018-08-02 19:57:45 +0100

    each instruction is added to the moventHistory, directions appear to be working

--- index.js
+++ index.js
@@ -40,21 +40,86 @@ movementHistory.push({
   direction: robotStartDirection
 })
 
-console.log(movementHistory);
+let _parseNewDirection = (lastDirection, instructionDirection) => {
+  let newDirection
+  switch (lastDirection) {
+    case 'N':
+      switch (instructionDirection) {
+        case 'R':
+          newDirection = 'E'
+          break
+        case 'L':
+          newDirection = 'W'
+          break
+      }
+      break
+
+    case 'E':
+      switch (instructionDirection) {
+        case 'R':
+          newDirection = 'S'
+          break
+        case 'L':
+          newDirection = 'N'
+          break
+      }
+      break
+
+    case 'S':
+      switch (instructionDirection) {
+        case 'R':
+          newDirection = 'W'
+          break
+        case 'L':
+          newDirection = 'E'
+          break
+      }
+      break
+
+    case 'W':
+      switch (instructionDirection) {
+        case 'R':
+          newDirection = 'N'
+          break
+        case 'L':
+          newDirection = 'S'
+          break
+      }
+      break
+  }
+  return newDirection
+}
 
 movementInstructions.map((instruction, index) => {
   console.log(`${index}: `, instruction)
   switch (instruction) {
     case 'R':
       console.log('Right turn')
+      movementHistory.push({
+        xPos: movementHistory[0].xPos, // last xPos
+        yPos: movementHistory[0].yPos, // last xPos
+        direction: _parseNewDirection(movementHistory[movementHistory.length - 1].direction, instruction)
+      })
       break
     case 'L':
       console.log('Left turn')
+      movementHistory.push({
+        xPos: movementHistory[0].xPos, // last xPos
+        yPos: movementHistory[0].yPos, // last xPos
+        direction: _parseNewDirection(movementHistory[movementHistory.length - 1].direction, instruction)
+      })
       break
     case 'F':
       console.log('Move "forward"')
+      movementHistory.push({
+        xPos: null, // last xPos
+        yPos: null, // last xPos
+        direction: movementHistory[0].direction
+      })
       break
   }
+
+  console.log('Last move: ', movementHistory[movementHistory.length - 1])
 })
 
 /* TODOs
