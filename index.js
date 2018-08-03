dc3f242132ea1f7d4a5b2de540ee9396ef63f403
Will Cook <hello@doubleucook.com>
2018-08-03 09:35:31 +0100

    correct output now if debugging is off

--- index.js
+++ index.js
@@ -51,58 +51,58 @@ let _parseNewDirection = (lastDirection, instructionDirection) => {
 }
 
 let _updateLocation = (previousLocation, direction) => {
-  console.log('previousLocation: ', previousLocation)
-  console.log('direction: ', direction)
+  // console.log('previousLocation: ', previousLocation)
+  // console.log('direction: ', direction)
   let newLocation
   switch (direction) {
     case 'N':
       // y++
       newLocation = previousLocation
       newLocation.yPos = parseInt(previousLocation.yPos) + 1
-      console.log('New location is +1 y? ', newLocation)
+      // console.log('New location is +1 y? ', newLocation)
       break
     case 'E':
       // x++
       newLocation = previousLocation
       newLocation.xPos = parseInt(previousLocation.xPos) + 1
-      console.log('New location is +1 x? ', newLocation)
+      // console.log('New location is +1 x? ', newLocation)
       break
     case 'S':
       // y--
       newLocation = previousLocation
       newLocation.yPos = parseInt(previousLocation.yPos) - 1
-      console.log('New location is -1 Y? ', newLocation)
+      // console.log('New location is -1 Y? ', newLocation)
       break
     case 'W':
       // x--
       newLocation = previousLocation
       newLocation.xPos = parseInt(previousLocation.xPos) - 1
-      console.log('New location is -1 X? ', newLocation)
+      // console.log('New location is -1 X? ', newLocation)
       break
   }
-  console.log('returning newlocation: ', newLocation)
+  // console.log('returning newlocation: ', newLocation)
   return newLocation
 }
 
 let _checkIfOffGrid = (newLocation) => {
   let offGrid = false
   if (newLocation.xPos > gridWorldWidth) {
-    console.log('Robot fell off the Eastern edge at: ', newLocation) // - 1  for last safe position??
+    // console.log('Robot fell off the Eastern edge at: ', newLocation) // - 1  for last safe position??
     offGrid = true
   }
   if (newLocation.yPos > gridWorldHeight) {
-    console.log('Robot fell off the Northern edge at: ', newLocation)
+    // console.log('Robot fell off the Northern edge at: ', newLocation)
     offGrid = true
   }
   if (newLocation.xPos < 0) {
-    console.log('Robot fell off the Southern edge at: ', newLocation)
+    // console.log('Robot fell off the Southern edge at: ', newLocation)
     offGrid = true
   }
   if (newLocation.yPos < 0) {
-    console.log('Robot fell off the Western edge at: ', newLocation)
+    // console.log('Robot fell off the Western edge at: ', newLocation)
     offGrid = true
   }
-  console.log('Robot is offGrid: ', offGrid)
+  // console.log('Robot is offGrid: ', offGrid)
   return offGrid
 }
 
@@ -119,13 +119,12 @@ let _checkForScent = (lastLocation) => {
       match = true
     }
   })
-  console.log('Previous robot been here? ', match)
+  // console.log('Previous robot been here? ', match)
   return match
 }
 
 /* TODOs
   - ensure all inputs are uppercase
-  - Add debugging
   - clean up output
 */
 
@@ -177,7 +176,7 @@ LLFFFLFLFL
 `
 
 let sampleInput = sampleInputAll
-let debugging = true
+let debugging = false
 
 sampleInput = sampleInput.replace(/\s/g, '') // remove whitespaces
 
@@ -192,7 +191,7 @@ if (debugging) { console.log('robotInstructions: ', robotInstructions) }
 let robots = robotInstructions.match(/\d\d[A-Z]+[^\d]/gi)
 
 robots.map((robot, index) => {
-  if (debugging) { console.log('Robot (', index , ') ',robot) }
+  if (debugging) { console.log('Robot (', index, ') ', robot) }
 
   let robotStartX = robot[0]
   // console.log('robotStartX: ', robotStartX);
@@ -202,7 +201,7 @@ robots.map((robot, index) => {
   // console.log('robotStartDirection: ', robotStartDirection);
 
   let movementInstructions = robot.substring(3).split('')
-  console.log('movementInstructions: ', movementInstructions);
+  if (debugging) { console.log('movementInstructions: ', movementInstructions) }
 
   let movementHistory = [] // store the last coordinates and direction
 
@@ -212,22 +211,22 @@ robots.map((robot, index) => {
     yPos: parseInt(robotStartY),
     direction: robotStartDirection
   })
-  console.log(movementHistory);
+  if (debugging) { console.log(movementHistory) }
 
   /* Run through the instructions */
   // movementInstructions.map((instruction, index) => {
   for (var i = 0; i < movementInstructions.length; i++) {
     let robotLost = false
     if (robotLost) {
-      console.log('SKIPPING ROBOT LOST');
+      if (debugging) { console.log('SKIPPING ROBOT LOST') }
       break
     }
 
     let instruction = movementInstructions[i]
-    console.log(`${i}: `, instruction)
+    if (debugging) { console.log(`${i}: `, instruction) }
     switch (instruction) {
       case 'R':
-        console.log('Right turn')
+        if (debugging) { console.log('Right turn') }
         movementHistory.push({
           xPos: movementHistory[movementHistory.length - 1].xPos, // last xPos
           yPos: movementHistory[movementHistory.length - 1].yPos, // last xPos
@@ -235,7 +234,7 @@ robots.map((robot, index) => {
         })
         break
       case 'L':
-        console.log('Left turn')
+        if (debugging) { console.log('Left turn') }
         movementHistory.push({
           xPos: movementHistory[movementHistory.length - 1].xPos, // last xPos
           yPos: movementHistory[movementHistory.length - 1].yPos, // last xPos
@@ -243,7 +242,7 @@ robots.map((robot, index) => {
         })
         break
       case 'F':
-        console.log('Move "forward"')
+        if (debugging) { console.log('Move "forward"') }
         // Detect if the robot has gone off the edge of the planet
         let newLocation = _updateLocation({
           xPos: movementHistory[movementHistory.length - 1].xPos,
@@ -258,18 +257,18 @@ robots.map((robot, index) => {
         )
         if (smellForScent) {
           // ignore moevemt, previous robot fell off here
-          console.log('ignoring instruction, do not want to fall off like the last one')
+          if (debugging) { console.log('ignoring instruction, do not want to fall off like the last one') }
           break
         }
 
         if (_checkIfOffGrid(newLocation) === true) { // no scent, robot is LOST
-          console.log('LOST!!!! adding to scents')
+          if (debugging) { console.log('LOST!!!! adding to scents') }
           scents.push({
             xPos: movementHistory[movementHistory.length - 1].xPos, // last xPos
             yPos: movementHistory[movementHistory.length - 1].yPos, // last xPos
             direction: movementHistory[movementHistory.length - 1].direction
           })
-          console.log('Updated scents: ', scents)
+          if (debugging) { console.log('Updated scents: ', scents) }
           robotLost = true
           break
         }
@@ -284,19 +283,13 @@ robots.map((robot, index) => {
 
     // console.log('Last position: ', movementHistory[movementHistory.length - 1])
     if (robotLost) {
-      console.log(movementHistory[movementHistory.length - 1].xPos.toString(),
-                  // ' ',
-                  movementHistory[movementHistory.length - 1].yPos.toString(),
-                  // ' ',
-                  movementHistory[movementHistory.length - 1].direction,
-                  'LOST')
+      console.log(`${movementHistory[movementHistory.length - 1].xPos.toString()} ${movementHistory[movementHistory.length - 1].yPos.toString()} ${movementHistory[movementHistory.length - 1].direction} LOST`)
       break
     }
     if (i === (movementInstructions.length - 1)) {
       if (debugging) { console.log('LAST ONE: ') }
       console.log(`${movementHistory[movementHistory.length - 1].xPos.toString()} ${movementHistory[movementHistory.length - 1].yPos.toString()} ${movementHistory[movementHistory.length - 1].direction}`)
-      console.log('_____')
+      if (debugging) { console.log('_____') }
     }
-    console.log(' ')
   }
 })
